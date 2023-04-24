2a1f9af232ef7ec41aaf8b872878dc29132b9335
shadeemerhi <shadmerhi@gmail.com>
2023-04-24 09:06:42 -0600

    remove import errors

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,62 +1,62 @@
 import prisma from "../prisma/client";
-import {
-  beerCategories,
-  beerItems,
-  foodCategories,
-  foodItems,
-  spiritCategories,
-  spiritItems,
-  wineCategories,
-  wineItems,
-} from "./seedData";
+// import {
+//   beerCategories,
+//   beerItems,
+//   foodCategories,
+//   foodItems,
+//   spiritCategories,
+//   spiritItems,
+//   wineCategories,
+//   wineItems,
+// } from "./seedData";
 
-const insertFoodCategories = async () => {
-  await prisma.foodCategory.createMany({
-    data: foodCategories,
-  });
-};
+// const insertFoodCategories = async () => {
+//   await prisma.foodCategory.createMany({
+//     data: foodCategories,
+//   });
+// };
 
-const insertFoodItems = async () => {
-  await prisma.foodItem.createMany({
-    data: foodItems,
-  });
-};
+// const insertFoodItems = async () => {
+//   await prisma.foodItem.createMany({
+//     data: foodItems,
+//   });
+// };
 
-const insertBeerCategories = async () => {
-  await prisma.beerCategory.createMany({
-    data: beerCategories,
-  });
-};
+// const insertBeerCategories = async () => {
+//   await prisma.beerCategory.createMany({
+//     data: beerCategories,
+//   });
+// };
 
-const insertBeerItems = async () => {
-  await prisma.beerItem.createMany({
-    data: beerItems,
-  });
-};
+// const insertBeerItems = async () => {
+//   await prisma.beerItem.createMany({
+//     data: beerItems,
+//   });
+// };
 
-const insertWineCategories = async () => {
-  await prisma.wineCategory.createMany({
-    data: wineCategories,
-  });
-};
+// const insertWineCategories = async () => {
+//   await prisma.wineCategory.createMany({
+//     data: wineCategories,
+//   });
+// };
 
-const insertWineItems = async () => {
-  await prisma.wineItem.createMany({
-    data: wineItems,
-  });
-};
+// const insertWineItems = async () => {
+//   await prisma.wineItem.createMany({
+//     data: wineItems,
+//   });
+// };
 
-const insertSpiritCategories = async () => {
-  await prisma.spiritCategory.createMany({
-    data: spiritCategories,
-  });
-};
+// const insertSpiritCategories = async () => {
+//   await prisma.spiritCategory.createMany({
+//     data: spiritCategories,
+//   });
+// };
 
-const insertSpiritItems = async () => {
-  await prisma.spiritItem.createMany({
-    data: spiritItems,
-  });
-};
+// const insertSpiritItems = async () => {
+//   await prisma.spiritItem.createMany({
+//     data: spiritItems,
+//   });
+// };
 
 async function main() {
   // await insertFoodCategories();
@@ -66,7 +66,7 @@ async function main() {
   //   await insertWineCategories();
   //   await insertWineItems();
   //   await insertSpiritCategories();
-  await insertSpiritItems();
+  // await insertSpiritItems();
 }
 
 main()
