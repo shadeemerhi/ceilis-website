e19d29db04d97213430624086b890982efef4a4a
shadeemerhi <shadmerhi@gmail.com>
2023-04-26 10:07:28 -0600

    add text escaping

--- src/app/components/common/NotAuthorized.tsx
+++ src/app/components/common/NotAuthorized.tsx
@@ -1,9 +1,9 @@
 const NotAuthorized = () => {
   return (
     <div className="flex flex-col gap-2 justify-center items-center p-6">
-      <span className="text-2xl">Not authorized</span>
+      <span className="text-2xl">{`Not authorized`}</span>
       <span className="text-lg">
-        Please contact Ceili's Administration to be granted access
+        {`Please contact Ceili's Administration to be granted access`}
       </span>
     </div>
   );
